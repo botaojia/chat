fbbd9aef80ccf9cff7f740b08538c2c6933aea1a
botao <botao.jia@gmail.com>
2017-11-08 00:11:59 -0500

    multithreaded chat server and client

--- chat_server.cpp
+++ chat_server.cpp
@@ -38,16 +38,14 @@ public:
 	static void run(std::shared_ptr<boost::asio::io_service> io_service) {
 		{
 			std::lock_guard < std::mutex > lock(m);
-			std::cout << "[" << std::this_thread::get_id() << "] Thread starts"
-					<< std::endl;
+			std::cout << "[" << std::this_thread::get_id() << "] Thread starts"	<< std::endl;
 		}
 
 		io_service->run();
 
 		{
 			std::lock_guard < std::mutex > lock(m);
-			std::cout << "[" << std::this_thread::get_id() << "] Thread ends"
-					<< std::endl;
+			std::cout << "[" << std::this_thread::get_id() << "] Thread ends" << std::endl;
 		}
 
 	}
@@ -60,15 +58,13 @@ std::mutex workerThread::m;
 
 class participant {
 public:
-	virtual ~participant() {
-	}
+	virtual ~participant() {}
 	virtual void onMessage(std::array<char, MAX_IP_PACK_SIZE> & msg) = 0;
 };
 
 class chatRoom {
 public:
-	void enter(std::shared_ptr<participant> participant,
-			const std::string & nickname) {
+	void enter(std::shared_ptr<participant> participant, const std::string & nickname) {
 		participants_.insert(participant);
 		name_table_[participant] = nickname;
 		std::for_each(recent_msgs_.begin(), recent_msgs_.end(),
@@ -80,8 +76,7 @@ public:
 		name_table_.erase(participant);
 	}
 
-	void broadcast(std::array<char, MAX_IP_PACK_SIZE>& msg,
-			std::shared_ptr<participant> participant) {
+	void broadcast(std::array<char, MAX_IP_PACK_SIZE>& msg,	std::shared_ptr<participant> participant) {
 		std::string timestamp = getTimestamp();
 		std::string nickname = getNickname(participant);
 		std::array<char, MAX_IP_PACK_SIZE> formatted_msg;
@@ -97,8 +92,7 @@ public:
 		}
 
 		std::for_each(participants_.begin(), participants_.end(),
-				boost::bind(&participant::onMessage, _1,
-						std::ref(formatted_msg)));
+				boost::bind(&participant::onMessage, _1, std::ref(formatted_msg)));
 	}
 
 	std::string getNickname(std::shared_ptr<participant> participant) {
@@ -106,47 +100,34 @@ public:
 	}
 
 private:
-	enum {
-		max_recent_msgs = 100
-	};
+	enum { max_recent_msgs = 100 };
 	std::unordered_set<std::shared_ptr<participant>> participants_;
 	std::unordered_map<std::shared_ptr<participant>, std::string> name_table_;
 	std::deque<std::array<char, MAX_IP_PACK_SIZE>> recent_msgs_;
 };
 
-class personInRoom: public participant, public std::enable_shared_from_this<
-		personInRoom> {
+class personInRoom: public participant,
+                    public std::enable_shared_from_this<personInRoom> {
 public:
 	personInRoom(boost::asio::io_service& io_service,
-			boost::asio::io_service::strand& strand, chatRoom& room) :
-			socket_(io_service), strand_(strand), room_(room) {
+			     boost::asio::io_service::strand& strand, chatRoom& room) :
+			     socket_(io_service), strand_(strand), room_(room) {
 	}
 
-	tcp::socket& socket() {
-		return socket_;
-	}
+	tcp::socket& socket() { return socket_;	}
 
 	void start() {
 		boost::asio::async_read(socket_,
-				boost::asio::buffer(nickname_, nickname_.size()),
-				strand_.wrap(
-						boost::bind(&personInRoom::nicknameHandler,
-								shared_from_this(), _1)));
+								boost::asio::buffer(nickname_, nickname_.size()),
+								strand_.wrap(boost::bind(&personInRoom::nicknameHandler, shared_from_this(), _1)));
 	}
 
 	void onMessage(std::array<char, MAX_IP_PACK_SIZE>& msg) {
 		bool write_in_progress = !write_msgs_.empty();
 		write_msgs_.push_back(msg);
 		if (!write_in_progress) {
-			std::cout << "[" << std::this_thread::get_id() << "]"
-					<< " onMessage, async_write :" << write_msgs_.front().data()
-					<< std::endl;
-			boost::asio::async_write(socket_,
-					boost::asio::buffer(write_msgs_.front(),
-							write_msgs_.front().size()),
-					strand_.wrap(
-							boost::bind(&personInRoom::writeHandler,
-									shared_from_this(), _1)));
+			boost::asio::async_write(socket_, boost::asio::buffer(write_msgs_.front(), write_msgs_.front().size()),
+					strand_.wrap(boost::bind(&personInRoom::writeHandler, shared_from_this(), _1)));
 		}
 	}
 
@@ -159,55 +140,40 @@ private:
 			nickname_[MAX_NICKNAME - 2] = ':';
 			nickname_[MAX_NICKNAME - 1] = ' ';
 		}
-		std::cout << nickname_.data() << std::endl;
+
 		room_.enter(shared_from_this(), std::string(nickname_.data()));
+
 		boost::asio::async_read(socket_,
 				boost::asio::buffer(read_msg_, read_msg_.size()),
-				strand_.wrap(
-						boost::bind(&personInRoom::readHandler,
-								shared_from_this(), _1)));
+				strand_.wrap(boost::bind(&personInRoom::readHandler, shared_from_this(), _1)));
 	}
 
 	void readHandler(const boost::system::error_code& error) {
-		std::cout << "chat_room handle_read " << read_msg_.data() << std::endl;
-		std::cout << " handle_read write_msgs_.size() " << write_msgs_.size()
-				<< std::endl;
 
 		if (!error) {
+
 			room_.broadcast(read_msg_, shared_from_this());
+
 			boost::asio::async_read(socket_,
 					boost::asio::buffer(read_msg_, read_msg_.size()),
-					strand_.wrap(
-							boost::bind(&personInRoom::readHandler,
-									shared_from_this(), _1)));
+					strand_.wrap(boost::bind(&personInRoom::readHandler, shared_from_this(), _1)));
 		} else {
 			room_.leave(shared_from_this());
 		}
 	}
 
 	void writeHandler(const boost::system::error_code& error) {
-		std::cout << " handle_write write_msgs_.size() " << write_msgs_.size()
-				<< std::endl;
+
 		if (!error) {
+
 			write_msgs_.pop_front();
-			std::cout << "[" << std::this_thread::get_id() << "]"
-					<< " handle_write, write_msgs_.size()="
-					<< write_msgs_.size() << std::endl;
 
 			if (!write_msgs_.empty()) {
-				std::cout << "[" << std::this_thread::get_id() << "]"
-						<< " handle_write, async_write "
-						<< write_msgs_.front().data() << std::endl;
 				boost::asio::async_write(socket_,
-						boost::asio::buffer(write_msgs_.front(),
-								write_msgs_.front().size()),
-						strand_.wrap(
-								boost::bind(&personInRoom::writeHandler,
-										shared_from_this(), _1)));
+						boost::asio::buffer(write_msgs_.front(), write_msgs_.front().size()),
+						strand_.wrap(boost::bind(&personInRoom::writeHandler, shared_from_this(), _1)));
 			}
 		} else {
-			std::cout << "[" << std::this_thread::get_id() << "]"
-					<< " handle_write, leave room " << std::endl;
 			room_.leave(shared_from_this());
 		}
 	}
@@ -225,20 +191,16 @@ public:
 	server(boost::asio::io_service& io_service,
 			boost::asio::io_service::strand& strand,
 			const tcp::endpoint& endpoint) :
-			io_service_(io_service), strand_(strand), acceptor_(io_service,
-					endpoint) {
+			io_service_(io_service), strand_(strand), acceptor_(io_service,	endpoint)
+    {
 		run();
-	}
+    }
 
 private:
 
 	void run() {
-		std::shared_ptr<personInRoom> new_participant(
-				new personInRoom(io_service_, strand_, room_));
-		acceptor_.async_accept(new_participant->socket(),
-				strand_.wrap(
-						boost::bind(&server::onAccept, this, new_participant,
-								_1)));
+		std::shared_ptr<personInRoom> new_participant(new personInRoom(io_service_, strand_, room_));
+		acceptor_.async_accept(new_participant->socket(), strand_.wrap(boost::bind(&server::onAccept, this, new_participant, _1)));
 	}
 
 	void onAccept(std::shared_ptr<personInRoom> new_participant,
@@ -265,28 +227,22 @@ int main(int argc, char* argv[]) {
 			return 1;
 		}
 
-		std::shared_ptr < boost::asio::io_service
-				> io_service(new boost::asio::io_service);
-		boost::shared_ptr < boost::asio::io_service::work
-				> work(new boost::asio::io_service::work(*io_service));
-		boost::shared_ptr < boost::asio::io_service::strand
-				> strand(new boost::asio::io_service::strand(*io_service));
+		std::shared_ptr < boost::asio::io_service> io_service(new boost::asio::io_service);
+		boost::shared_ptr < boost::asio::io_service::work> work(new boost::asio::io_service::work(*io_service));
+		boost::shared_ptr < boost::asio::io_service::strand> strand(new boost::asio::io_service::strand(*io_service));
 
-		std::cout << "[" << std::this_thread::get_id() << "]" << "server starts"
-				<< std::endl;
+		std::cout << "[" << std::this_thread::get_id() << "]" << "server starts" << std::endl;
 
 		std::list < std::shared_ptr < server >> servers;
 		for (int i = 1; i < argc; ++i) {
 			tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[i]));
-			std::shared_ptr<server> a_server(
-					new server(*io_service, *strand, endpoint));
+			std::shared_ptr<server> a_server(new server(*io_service, *strand, endpoint));
 			servers.push_back(a_server);
 		}
 
 		boost::thread_group workers;
 		for (int i = 0; i < 1; ++i) {
-			boost::thread * t = new boost::thread { boost::bind(
-					&workerThread::run, io_service) };
+			boost::thread * t = new boost::thread { boost::bind(&workerThread::run, io_service) };
 
 #ifdef __linux__
 			// bind cpu affinity for worker thread in linux
